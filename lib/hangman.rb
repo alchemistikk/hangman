b2cabf7065d982b49c769d2d1713623098d507be
Mike Mitchell <alchemistikk@gmail.com>
2021-11-11 14:03:24 -0500

    Add update_board method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -7,10 +7,11 @@ word_arr.each do |word|
   refined_word_arr.push(word) if word.length >= 5 && word.length <= 12
 end
 
+# An instantion of the class Game will maintain the state of a game of hangman
 class Game
   attr_reader :secret_word
   attr_accessor :guesses, :board
-  
+
   def initialize(guesses, words)
     @secret_word = words.sample
     @guesses = guesses
@@ -18,27 +19,30 @@ class Game
   end
 
   def play
-    @secret_word.each_char do |letter|
-      @board.concat(' _')
-    end
+    @secret_word.each_char { @board.concat('_') }
     while @guesses.positive?
       puts "Guesses remaining: #{@guesses}"
       play_round
-      @guesses -= 1
     end
+    puts @secret_word
   end
 
   def play_round
     guess = gets.chomp
-    puts 'DOPE. YOU GOT IT.' if secret_word.include?(guess)
-    puts board
-  end  
-end
+    if @secret_word.include?(guess)
+      update_board(guess)
+    else
+      @guesses -= 1
+    end
+    puts @board
+  end
 
-# You should also display which correct letters have already been chosen
-# (and their position in the word, e.g. _ r o g r a _ _ i n g) 
-# and which incorrect letters have already been chosen.
+  def update_board(guess)
+    @secret_word.split('').each_with_index do |v, i|
+      @board[i] = guess if v == guess
+    end
+  end
+end
 
 game = Game.new(4, refined_word_arr)
 game.play
-
