8bc55b7c3c5c7ee102607f5179867c8099cc0943
Mike Mitchell <alchemistikk@gmail.com>
2021-11-11 12:58:42 -0500

    Refactor methods into Game class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 contents = File.read('5desk.txt')
 word_arr = contents.split("\r\n")
@@ -7,16 +7,38 @@ word_arr.each do |word|
   refined_word_arr.push(word) if word.length >= 5 && word.length <= 12
 end
 
-def play_game(guesses, words)
-  secret_word = words.sample
-  guesses_remaining = guesses
-  play_round(secret_word, guesses_remaining)
+class Game
+  attr_reader :secret_word
+  attr_accessor :guesses, :board
+  
+  def initialize(guesses, words)
+    @secret_word = words.sample
+    @guesses = guesses
+    @board = ''
+  end
+
+  def play
+    @secret_word.each_char do |letter|
+      @board.concat(' _')
+    end
+    while @guesses.positive?
+      puts "Guesses remaining: #{@guesses}"
+      play_round
+      @guesses -= 1
+    end
+  end
+
+  def play_round
+    guess = gets.chomp
+    puts 'DOPE. YOU GOT IT.' if secret_word.include?(guess)
+    puts board
+  end  
 end
 
-def play_round(secret_word, guesses_remaining)
-  guess = gets.chomp
-  puts "DOPE. YOU GOT IT." if secret_word.include?(guess)
-  guesses_remaining -= 1
-end
+# You should also display which correct letters have already been chosen
+# (and their position in the word, e.g. _ r o g r a _ _ i n g) 
+# and which incorrect letters have already been chosen.
 
-play_game(4, refined_word_arr)
+game = Game.new(4, refined_word_arr)
+game.play
+
